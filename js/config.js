467c86daddef612f9e1786e12f2e444b2136903d
techservicepeople70 <techservicepeople70@gmail.com>
2025-12-01 00:34:24 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -5,7 +5,8 @@ window._config = {
         region: 'us-east-2' // e.g. us-east-2
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://qyoer0lorc.execute-api.us-east-2.amazonaws.com/Dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
